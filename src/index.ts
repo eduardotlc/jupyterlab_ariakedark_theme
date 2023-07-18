caad8bf4669b829619644238fa0328235ddf2d00
eduardotc <eduardotcampos@usp.br>
2023-07-18 05:36:55 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the jupyterlab_ariakedark_theme extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab_ariakedark_theme:plugin',
+  description: 'An ariake dark palette based JupyterLab theme extension.',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension jupyterlab_ariakedark_theme is activated!');
+    const style = 'jupyterlab_ariakedark_theme/index.css';
+
+    manager.register({
+      name: 'jupyterlab_ariakedark_theme',
+      isLight: false,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default plugin;
