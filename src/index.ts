7c30e0c63c738d3fea334f3e4aeb97fd7d6f74df
eduardotlc <eduardotcampos@usp.br>
2025-10-17 20:56:29 -0300

    linting build fails format errors fix

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+} from "@jupyterlab/application";
 
-import { IThemeManager } from '@jupyterlab/apputils';
+import { IThemeManager } from "@jupyterlab/apputils";
 
 /**
  * Initialization data for the jupyterlab_ariakedark_theme extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_ariakedark_theme:plugin',
-  description: 'An ariake dark palette based JupyterLab theme extension.',
+  id: "jupyterlab_ariakedark_theme:plugin",
+  description: "An ariake dark palette based JupyterLab theme extension.",
   autoStart: true,
   requires: [IThemeManager],
   activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
     console.log(
-      'JupyterLab extension jupyterlab_ariakedark_theme is activated!'
+      "JupyterLab extension jupyterlab_ariakedark_theme is activated!"
     );
-    const style = 'jupyterlab_ariakedark_theme/index.css';
+    const style = "jupyterlab_ariakedark_theme/index.css";
 
     manager.register({
-      name: 'Ariake Dark',
+      name: "Ariake Dark",
       isLight: false,
       load: () => manager.loadCSS(style),
       unload: () => Promise.resolve(undefined)
